2ae764805a225c9d93c818371f06af7646ca5058
Masshiros <vunngcs220106@fpt.edu.vn>
2025-08-28 21:04:33 +0700

    fix refresh token no need access token

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -5,8 +5,8 @@ const { authentication } = require("../../middlewares/auth");
 const router = express.Router();
 router.post("/shop/signup", asyncHandler(accessController.signUp));
 router.post("/shop/signin", asyncHandler(accessController.signIn));
+router.post("/shop/refreshToken", asyncHandler(accessController.refreshToken));
 // auth route
 router.use(authentication);
 router.post("/shop/signout", asyncHandler(accessController.signOut));
-router.post("/shop/refreshToken", asyncHandler(accessController.refreshToken));
 module.exports = router;
